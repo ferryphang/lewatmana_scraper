1a5840a9d96d31e6931ba313668e26ee9250e04a
FerryPhang <ferry.pangestu@ymail.com>
2014-12-09 10:05:04 +0700

    better naming

--- clock.rb
+++ clock.rb
@@ -1,14 +1,19 @@
 require 'clockwork'
-require_relative 'scraper'
+require_relative 'lewat_mana'
   
 module Clockwork
   handler do |job|
     puts "Running #{job}"
   end
 
-  every(1.minute, 'Scrape Lewatmana.com') {
+  every(1.minute, 'Scrape MOI Kelapa Gading CCTV') {
     moi = LewatMana.new "http://lewatmana.com/cam/209/moi-kelapa-gading/", "/data"
     moi.scrape
+  }
+
+  every(5.minutes, 'Build GIF') {
+    project_dir = File.expand_path(File.dirname(__FILE__))
+    system("convert #{project_dir + '/data/*.jpg' } #{project_dir + '/timelapse/MOI.gif'}")
   }
 
 end
